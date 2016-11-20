40b0931e369e7ac5f5a6a17a565dad7ac30a2347
juniorsolo <junior.solo@hotmail.com>
2016-11-20 07:32:55 -0300

    Termino da aula 10 criando diretiva.

--- js/directive/uiAlertDirective.js
+++ js/directive/uiAlertDirective.js
@@ -1,5 +1,17 @@
 angular.module("listaTelefonica").directive("uiAlert", function() {
 	return {
-		templateUrl: "view/alert.html"
+		templateUrl: "view/alert.html",
+		replace: true,
+		restrict: "AE",
+		// definindo o scopo ela perde o acesso ao controller (id = 2 ) diretiva (id= 3), perde o acesso
+		// ao $scope.error
+		scope: {
+			  // como os nomes são os mesmo pode ser usado apenas o "@" que funciona
+              title: "@title",
+              // como o atributo "=" ocorre o toDataBind ficando biDirecional
+              // se ambos tiverem o mesmo nome pode colocar somente o "=" que funciona
+              //mensagem: "=message"
+		},
+		transclude: true
 	};
 });
